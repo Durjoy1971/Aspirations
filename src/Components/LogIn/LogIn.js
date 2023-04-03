c7f278d3b87281f9c0aed0a37c1b4711216f7003
Durjoy1971 <91456847+Durjoy1971@users.noreply.github.com>
2023-04-03 18:04:20 +0600

    Add files via upload
    Added Admin Login
    Slightly changes LogIn and Navigation

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -4,23 +4,20 @@ import './LogIn.css';
 const LogIn = () => {
   return (
     <section className="section-Login">
-      <div className="form-box">
+      <div className="form-box-Login">
         <div className="form-value">
           <form action="">
             <h2 className="h2-Login">Login</h2>
-            <div className="inputbox">            
+            <div className="inputbox-Login">            
               <input type="email" required />
               <label>Email</label>
             </div>
-            <div className="inputbox">
+            <div className="inputbox-Login">
               <input type="password" required />
               <label>Password</label>
             </div>
-            <div className="forget">
-
-            </div>
-            <button>Log in</button>
-            <div className="register">
+            <button className="button-login">Log in</button>
+            <div className="register-Login">
               <p>
                 Don't have a account <a href="/register">Register</a>
               </p>
